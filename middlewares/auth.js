694fc32de424187a339bb8508eefbb649ef1cbb4
ManmohitSingh <dhimanmohit408@gmail.com>
2025-06-15 13:02:28 +0100

    minor fixes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,21 @@
-import { getUser } from '../service/auth.js'
+import { getUser } from '../service/auth.js';
 
-
-async function restrictToLoggedinUserOnly(req,res,next){
-const userUid = req.cookies ? req.cookies.uid : undefined;
-if(!userUid) return res.redirect('/login');
-
-const user = getUser(userUid);
-if(!user) return res.redirect('/login');
-
-req.user = user;
-next();
-} 
-
-async function checkAuth(req,res,next){
+async function restrictToLoggedinUserOnly(req, res, next) {
     const userUid = req.cookies ? req.cookies.uid : undefined;
-    const user = getUser(userUid);
+    if (!userUid) return res.redirect('/login');
+
+    const user = getUser(userUid);  // OK if getUser is sync
+    if (!user) return res.redirect('/login');
+
     req.user = user;
     next();
 }
-export { restrictToLoggedinUserOnly, checkAuth }  
+
+async function checkAuth(req, res, next) {
+    const userUid = req.cookies ? req.cookies.uid : undefined;
+    const user = getUser(userUid);  // OK if getUser is sync
+    req.user = user;
+    next();
+}
+
+export { restrictToLoggedinUserOnly, checkAuth };
